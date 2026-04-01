c558d83e78c579913a0613b4cb4bf77d86c125cf
Gena <genada89@gmail.com>
2026-04-01 21:23:43 +0300

    solution delete code dulicate

--- app/main.py
+++ app/main.py
@@ -11,11 +11,10 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors":
                 [format_linter_error(error) for error in errors],
-            "path": file_path, "status": "failed"
+            "path": file_path, "status": "failed" if errors else "passed"
             }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(path, errors) if errors
-            else {"errors": [], "path": path, "status": "passed"}
+    return [format_single_linter_file(path, errors)
             for path, errors in linter_report.items()]
